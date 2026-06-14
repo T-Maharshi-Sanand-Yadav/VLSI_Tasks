c55fa3e144e147ec2e30c162bf5765a0dde6c5e8
vsrikanth46 <vsrikanth0406@gmail.com>
2026-06-14 22:05:20 +0530

    adding reset_sync_2ff_tb.v

---
+++ Assignment_01_Synchronizer/Student_Submissions/Student_02/TESTBENCH/reset_sync_2ff_tb.v
@@ -0,0 +1,43 @@
+module reset_sync_2ff_tb;
+
+reg CLK;
+reg D;
+reg RST_N;
+wire Q;
+wire QB;
+
+reset_sync_2ff DUT(
+    .CLK(CLK),
+    .D(D),
+    .RST_N(RST_N),
+    .Q(Q),
+    .QB(QB)
+);
+
+initial
+begin
+    CLK = 0;
+    forever #5 CLK = ~CLK;
+end
+
+initial
+begin
+    $dumpfile("reset_sync_2ff.vcd");
+  $dumpvars(0,reset_sync_2ff_tb);
+
+    RST_N = 0;
+    D = 0;
+
+    #12 RST_N = 1;
+
+    #8  D = 1;
+    #20 D = 0;
+    #15 D = 1;
+
+    #10 RST_N = 0;
+    #10 RST_N = 1;
+
+    #50 $finish;
+end
+
+endmodule
